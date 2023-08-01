e5ba2d102f7d6727fd66e0052fc3caca7779e8b5
nyximos <nyximos@gmail.com>
2023-08-01 22:39:41 +0900

    feat: 한 줄씩 출력

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,15 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 fun main() {
-    println("Hello, World!")
+    val inputStream = object {}.javaClass.getResourceAsStream("/popular_keywords.csv")
+
+    BufferedReader(InputStreamReader(inputStream)).use { reader ->
+        var line: String? = reader.readLine()
+        while (line != null) {
+            println(line)
+
+            line = reader.readLine()
+        }
+    }
 }
