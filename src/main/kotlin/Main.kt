44455a14a59b4ad214debf1dbe83a97095de3651
nyximos <nyximos@gmail.com>
2023-08-01 23:44:21 +0900

    add: Logger

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,16 @@
+import org.slf4j.LoggerFactory
 import java.io.File
 
 fun main() {
     val keywordsFilePath = "src/main/resources/popular_keywords.csv"
+    val logger = LoggerFactory.getLogger("Top Searched")
+
 
     File(keywordsFilePath).bufferedReader().use { reader ->
         reader.readLine()
 
         for (line in reader.lines()) {
-            println(line)
+            logger.info(line)
         }
     }
 }
