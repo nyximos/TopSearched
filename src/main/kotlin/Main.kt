7a0b294920d5618df0bdd83c776ea19b3353e5e9
nyximos <nyximos@gmail.com>
2023-08-01 23:12:58 +0900

    refactor: use bufferedReader()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,9 @@
-import java.io.BufferedReader
 import java.io.File
-import java.io.InputStreamReader
 
 fun main() {
-    val fileName = "src/main/resources/popular_keywords.csv"
-    val file = File(fileName)
-    val inputStream = file.inputStream()
+    val keywordsFilePath = "src/main/resources/popular_keywords.csv"
 
-    BufferedReader(InputStreamReader(inputStream)).use { reader ->
+    File(keywordsFilePath).bufferedReader().use { reader ->
         var line: String? = reader.readLine()
         while (line != null) {
             println(line)
