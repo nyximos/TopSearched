f03da901d1a8bca099f6ffd63fb2b61b41fdc22c
nyximos <nyximos@gmail.com>
2023-08-02 22:54:17 +0900

    refactor: Functional Programming

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,38 +1,27 @@
 import java.io.File
+import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import org.slf4j.LoggerFactory
-import java.time.LocalDateTime
 
 fun main() {
     val keywordsFilePath = "src/main/resources/popular_keywords.csv"
-    val map = mutableMapOf<LocalTime, MutableList<String>>()
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
     val currentTime = LocalTime.now().withHour(0).withNano(0)
 
     val logger = LoggerFactory.getLogger("Top Searched")
 
-    File(keywordsFilePath).bufferedReader().use { reader ->
-        reader.readLine()
-
-        reader.lines().forEach { line ->
-            val (key, value) = line.split(",")
-            val dateTime = LocalDateTime.parse(key, formatter)
-            val localTime = dateTime.toLocalTime().withHour(0)
-
-            if (localTime.isAfter(currentTime.minusMinutes(5)) && localTime.isBefore(currentTime)) {
-                map.computeIfAbsent(localTime) { mutableListOf() }.add(value)
-            }
-        }
+    val trends = File(keywordsFilePath).bufferedReader().useLines { lines ->
+        lines.drop(1) // Skip the header
+            .map { line -> line.split(",") }
+            .map { (key, value) -> Pair(LocalDateTime.parse(key, formatter).toLocalTime().withHour(0), value) }
+            .filter { (localTime, _) -> localTime.isAfter(currentTime.minusMinutes(5)) && localTime.isBefore(currentTime) }
+            .groupBy { (_, value) -> value }
+            .mapValues { (_, values) -> values.size }
+            .toList()
+            .sortedByDescending { (_, count) -> count }
+            .take(10)
     }
-
-    val trends = map.values
-        .flatten()
-        .groupingBy { it }
-        .eachCount()
-        .toList()
-        .sortedByDescending { it.second }
-        .take(10)
 
     trends.forEach { (value, count) ->
         logger.info("$value: $count ")
