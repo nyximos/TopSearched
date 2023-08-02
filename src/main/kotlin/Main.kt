29ac64121e4439ad8eacaffa12eac96a8318324e
nyximos <nyximos@gmail.com>
2023-08-02 22:06:41 +0900

    feat: Top 10

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,14 @@
-import org.slf4j.LoggerFactory
 import java.io.File
-import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
+import org.slf4j.LoggerFactory
+import java.time.LocalDateTime
 
 fun main() {
     val keywordsFilePath = "src/main/resources/popular_keywords.csv"
-    val map = mutableMapOf<LocalTime, String>()
+    val map = mutableMapOf<LocalTime, MutableList<String>>()
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    val currentTime = LocalTime.now().withHour(0).withNano(0)
 
     val logger = LoggerFactory.getLogger("Top Searched")
 
@@ -17,14 +18,23 @@ fun main() {
         for (line in reader.lines()) {
             val (key, value) = line.split(",")
             val dateTime = LocalDateTime.parse(key, formatter)
-            map[dateTime.toLocalTime()] = value
+            val localTime = dateTime.toLocalTime().withHour(0)
+
+            if (localTime.isAfter(currentTime.minusMinutes(5)) && localTime.isBefore(currentTime)) {
+                map.computeIfAbsent(localTime) { mutableListOf() }.add(value)
+            }
         }
     }
-    printMap(map, logger)
-}
 
-fun printMap(map: Map<LocalTime, String>, logger: org.slf4j.Logger) {
-    map.forEach { (key, value) ->
-        logger.info("$key -> $value")
+    val trends = map.values
+        .flatten()
+        .groupingBy { it }
+        .eachCount()
+        .toList()
+        .sortedByDescending { it.second }
+        .take(10)
+
+    for ((value, count) in trends) {
+        logger.info("$value: $count ")
     }
 }
